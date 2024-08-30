366d07af5bea2325be87d0c4d39d29251d3ab789
David Tang <687449+iamdtang@users.noreply.github.com>
2024-08-29 15:16:28 -1000

    Count component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Count from "./Count";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      Count: {count}
+      <Count value={count} />
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
     </div>
